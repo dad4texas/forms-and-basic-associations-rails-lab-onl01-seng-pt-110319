a1179c81a5043c775a1019e5026d975ada6c6433
Warren Harper II <warrenharper2@att.net>
2020-04-08 22:28:14 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -5,35 +5,45 @@ class Song < ActiveRecord::Base
   belongs_to :genre
   has_many :notes
 
-
   def genre_name=(genre)
-   self.genre = Genre.find_or_create_by(name: genre)
-  end
-
- def genre_name
-  self.genre.name if self.genre
- end
-
- def artist_name
-  self.artist.name if self.artist
- end
-
- def artist_name=(name)
-  self.artist = Artist.find_or_create_by(name: name)
- end
-
- def note_contents=(content)
-  content.each do |content|
-    self.notes << Note.find_or_create_by(content: content)
-  end
-end
-
-def note_contents
-  ret = []
-  self.notes.each do |note|
-    ret << note.content unless note.content.empty?
-  end
-  ret
-end
-
+      self.genre = Genre.find_or_create_by(name: genre)
+    end
+  
+    def genre_name
+      self.genre.name if self.genre
+    end
+  
+    def artist_name
+      self.artist.name if self.artist
+    end
+  
+    def artist_name=(name)
+      self.artist = Artist.find_or_create_by(name: name)
+    end
+  
+    def note_contents=(notes)
+      notes.each do |note|
+        self.notes.build(content: note).save if note.length > 0
+      end
+    end
+  
+    def note_contents
+      self.notes.map do |note|
+        note.content
+      end
+    end
+  
+    def note_ids=(ids)
+      ids.each do |id|
+        id_num = id.chars.last.to_i
+        note = Note.find_or_create_by(id: id_num)
+        self.notes << note
+      end
+    end
+  
+    def note_ids
+      self.notes.map do |note|
+        "song_notes_#{note.id}"
+      end
+   end
 end

e33e0e2eedcf6dbb17f50998b42b3bb68572f97d
Warren Harper II <warrenharper2@att.net>
2020-04-08 22:28:17 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -8,31 +8,31 @@ class Song < ActiveRecord::Base
   def genre_name=(genre)
       self.genre = Genre.find_or_create_by(name: genre)
     end
-  
+
     def genre_name
       self.genre.name if self.genre
     end
-  
+
     def artist_name
       self.artist.name if self.artist
     end
-  
+
     def artist_name=(name)
       self.artist = Artist.find_or_create_by(name: name)
     end
-  
+
     def note_contents=(notes)
       notes.each do |note|
         self.notes.build(content: note).save if note.length > 0
       end
     end
-  
+
     def note_contents
       self.notes.map do |note|
         note.content
       end
     end
-  
+
     def note_ids=(ids)
       ids.each do |id|
         id_num = id.chars.last.to_i
@@ -40,7 +40,7 @@ class Song < ActiveRecord::Base
         self.notes << note
       end
     end
-  
+
     def note_ids
       self.notes.map do |note|
         "song_notes_#{note.id}"

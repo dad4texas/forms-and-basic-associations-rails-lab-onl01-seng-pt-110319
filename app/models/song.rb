1a717c441c147af0a2d9d059dcba37ff3979ec12
Warren Harper II <warrenharper2@att.net>
2020-04-08 19:12:09 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -8,7 +8,7 @@ class Song < ActiveRecord::Base
 
   def genre_name=(genre)
    self.genre = Genre.find_or_create_by(name: genre)
- end
+  end
 
  def genre_name
   self.genre.name if self.genre
@@ -16,13 +16,13 @@ class Song < ActiveRecord::Base
 
  def artist_name
   self.artist.name if self.artist
-end
+ end
 
-def artist_name=(name)
+ def artist_name=(name)
   self.artist = Artist.find_or_create_by(name: name)
-end
+ end
 
-def note_contents=(content)
+ def note_contents=(content)
   content.each do |content|
     self.notes << Note.find_or_create_by(content: content)
   end
